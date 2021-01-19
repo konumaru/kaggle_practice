2a81117ec42d2e9a0fb9fd05924b12e9b7fdfb6e
konumaru <konumaru1022@gmail.com>
2021-01-19 13:06:08 +0900

    [add] create gce env feature.
    you can make gce enviroment by make commnand.

---
+++ gcp_startup_script.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+
+sudo apt update
+sudo apt install -y cuda
+sudo apt install -y build-essential \
+    libffi-dev \
+    zlib1g-dev \
+    liblzma-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    python-pip \
+    make \
+    wget \
+    curl \
+    llvm \
+    libncurses5-dev \
+    libncursesw5-dev \
+    xz-utils \
+    tk-dev \
+    libffi-dev \
+    libssl-devn
