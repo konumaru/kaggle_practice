a11351d4d6d9a56293b556202fc750f60c4c1aed
konumaru <konumaru1022@gmail.com>
2021-02-22 14:48:51 +0900

    [update] adupte updated mikasa

--- tests/trainer/test_cross_validation.py
+++ tests/trainer/test_cross_validation.py
@@ -25,7 +25,8 @@ def test_cv_trainer(load_regression_dataset):
     )
     trainer = CrossValidationTrainer(cv, base_trainer)
     trainer.fit(X_train, y_train)
-    y_pred = np.mean(trainer.predict(X_test), axis=0)
+    y_pred_all = trainer.predict(X_test)
+    y_pred = np.mean(y_pred_all, axis=0)
     assert y_test.shape == y_pred.shape
 
     score = metrics.mean_squared_error(y_test, y_pred)
@@ -52,13 +53,16 @@ def test_rsa_cv_trainer(load_regression_dataset):
     y_train = pd.Series(y_train.ravel())
     y_test = pd.Series(y_test.ravel())
 
-    cv = KFold(n_splits=3, shuffle=True, random_state=42)
+    num_split = 3
+    num_seed = 3
+    cv = KFold(n_splits=num_split, shuffle=True, random_state=42)
     base_trainer = SklearnRegressionTrainer(
         model=RandomForestRegressor(n_estimators=10)
     )
     trainer = RSACVTrainer(cv, base_trainer)
-    trainer.fit(X_train, y_train, random_state=42, num_seed=3)
-    y_pred = np.mean(trainer.predict(X_test), axis=0)
+    trainer.fit(X_train, y_train, random_state=42, num_seed=num_seed)
+    y_pred_all = trainer.predict(X_test)
+    y_pred = np.mean(y_pred_all, axis=0)
     assert y_test.shape == y_pred.shape
 
     score = metrics.mean_squared_error(y_test, y_pred)
@@ -66,6 +70,7 @@ def test_rsa_cv_trainer(load_regression_dataset):
     (name, mean_importance, std_importance) = trainer.get_importance()
     # Get models.
     models = trainer.get_model()
+    assert len(models) == (num_split * num_seed)
     # Get oof and target.
     oof = trainer.get_cv_oof()
     target = trainer.get_cv_targets()
